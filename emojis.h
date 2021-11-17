83216637b69bc48606138a9f75c0c72d5a6ce1ac
rbartoncello <rbartoncello@fi.uba.ar>
2021-11-17 18:59:29 -0300

    agregado de emojis.h

---
+++ emojis.h
@@ -0,0 +1,51 @@
+#ifndef EMOJIS_H
+#define EMOJIS_H
+#include <string>
+
+using namespace std;
+
+const string EMOJI_LOANDING = "\U0001f503";
+const string EMOJI_WARNRING = "\U0001f6ab";
+const string EMOJI_HECHO = "\u2705";
+const string EMOJI_MAL = "\u274C";
+
+const string EMOJI_EDIFICIO_CONSTRUCION = "\U0001f3d7";
+const string EMOJI_EDIFICIO = "\U0001f3ec";
+const string EMOJI_MARTILLO = "\U0001f528";
+const string EMOJI_PARED = "\U0001f9f1";
+
+const string EMOJI_RELOJ = "\u23F1";
+const string EMOJI_BUSQUEDA = "\U0001f50d";
+const string EMOJI_LISTA = "\U0001f4c4";
+
+const string EMOJI_MAPA = "\U0001f5fa";
+const string EMOJI_BRUJULA = "\U0001f9ed";
+const string EMOJI_INVENTARIO = "\U0001f9f0";
+const string EMOJI_GUARDA_SALIR = "\U0001f4be";
+
+const string EMOJI_LLUVIA = "\U0001f327";
+const string EMOJI_LLUVIA_CON_TRUENOS = "\u26C8";
+
+const string EMOJI_PIEDRA = "\U0001faa8";
+const string EMOJI_MADERA = "\U0001fab5";
+const string EMOJI_METAL = "\u2699";
+
+const string EMOJI_MINA ="\u26CF ";
+const string EMOJI_ASERRADERO ="\U0001fa93";
+const string EMOJI_FABRICA ="\U0001f3ed";
+const string EMOJI_ESCUELA ="\U0001f3eb";
+const string EMOJI_OBELISCO ="\U0001f5fc";
+const string EMOJI_PLANTA_ENERGIA ="\u26A1";
+
+const string EMOJI_UNO = "1\u20E3 ";
+const string EMOJI_DOS = "2\u20E3 ";
+const string EMOJI_TRES = "3\u20E3 ";
+const string EMOJI_CUATRO = "4\u20E3 ";
+const string EMOJI_CINCO = "5\u20E3 ";
+const string EMOJI_SEIS = "6\u20E3 ";
+const string EMOJI_SIETE = "7\u20E3 ";
+const string EMOJI_OCHO = "8\u20E3 ";
+const string EMOJI_NUEVE = "9\u20E3 ";
+const string EMOJI_DIEZ = "1\u20E30\u20E3";
+
+#endif
